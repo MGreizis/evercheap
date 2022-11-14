7ab28f686caebc5949a5b35fe74b935c828a92da
Martins Greizis <grei0003@hz.nl>
2022-11-14 14:47:09 +0100

    very small refactor

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,16 +3,16 @@ import Head from 'next/head'
 export default function About() {
     return (
         <>
-        <Head>
-            <title>About</title>
-            <meta name="description" content="About page" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    
-        <main>
-            <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600'>About page</h1>
-            <p>This is a test page</p>
-        </main>
+            <Head>
+                <title>About</title>
+                <meta name="description" content="About page" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        
+            <main>
+                <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600'>About page</h1>
+                <p>This is a test page</p>
+            </main>
         </>
     )
     }
