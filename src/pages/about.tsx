5f75c1a0eca6c066527e002de3f77e8b7be22c46
Martins Greizis <grei0003@hz.nl>
2022-11-15 11:28:27 +0100

    really quick page title change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function About() {
-    return (
-        <>
-            <Head>
-                <title>About</title>
-                <meta name="description" content="About page" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        
-            <main>
-                <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600'>About page</h1>
-                <p>This is a test page.</p>
-            </main>
-        </>
-    )
-    }
+  return (
+    <>
+      <Head>
+        <title>Evercheap</title>
+        <meta name="description" content="About page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
+          About page
+        </h1>
+        <p>This is a test page.</p>
+      </main>
+    </>
+  );
+}
