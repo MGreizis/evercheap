51b96a92972e4f1e62a97588633e7dc3e18c247f
JaneVick01 <ziak0001@hz.nl>
2022-12-02 11:35:23 +0100

    combobox fail

--- nextjsapi/pages/index.jsx
+++ nextjsapi/pages/index.jsx
@@ -1,10 +1,14 @@
-import styles from '../styles/Home.module.css';
 import React, { useState, useEffect } from 'react';
+import Combobox from "react-widgets/Combobox";
 
 export default function Home() {
+
+
 	const [error, setError] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [stores, setShops] = useState([]);
+// using use state hook to update the state variable
+	const [shop, setShop] = useState();
 
 	useEffect(() => {
 		fetch("http://localhost:3010/stores")
@@ -13,7 +17,9 @@ export default function Home() {
 				(data) => {
 					setIsLoaded(true);
 					setShops(data);
+					setShop(data);
 					console.log(data);
+					console.log(setShop(data));
 				},
 				(error) => {
 					setIsLoaded(true);
@@ -26,19 +32,46 @@ export default function Home() {
 	} else if (!isLoaded) {
 		return <div>Loading...</div>;
 	} else {
-		return (
-			<div className={styles.container}>
-				<ul>
-				{stores.data.map(store => (
-					<li key={store.id} className={styles.main}>
-						<div>
-							<h1>{store.name}</h1>
-              				<h1>{store.id}</h1>
-						</div>
-					</li>
-				))}
-				</ul>
-			</div>
+		return (	
+			<><div>
+
+				<button onClick={callStore(shop)}>Potvrdiť</button>
+				{/* <select id="shops" value={shop}
+						onChange={(e) => setShop(e.value)}>
+							<option value="lidl">lidl</option>
+							<option value="ah">ah</option>
+							<option value="jumbo">jumbo</option>
+						</select>
+						<h1>Selected shop: {shop}</h1>
+						</div> */}
+
+				<Combobox
+					hideCaret
+					hideEmptyPopup
+					data={stores.data.map(store => (store.name))}
+					value={shop.name} // set selected value
+					onChange={(e) => setShop(e.target.value)}
+  
+/>
+			</div><div>
+					<ul>
+						{stores.data.map(store => (
+							<li key={store.id}>
+								<div>
+									<h1>{store.name}</h1>
+									<h1>{store.id}</h1>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div></>
 		);
 	}
+}
+
+function callStore(nazov) {
+console.log("tu som")
+
+
+	
 }
