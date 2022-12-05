ee472c82feab3a0b6b22e28a23ba820aebd300ca
Martins Greizis <grei0003@hz.nl>
2022-12-05 16:05:03 +0100

    base layout for marketplace is done

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -3,9 +3,10 @@ import Header from "../../components/header";
 import Footer from "../../components/footer";
 import BackgroundBlobOrange from "../../components/backgroundBlobOrange";
 import BackgroundBlobWhite from "../../components/backgroundBlobWhite";
-import Styles from "../../../styles/Home.module.css";
+import Styles from "../../../styles/Marketplace.module.css";
 import { useSession } from "@supabase/auth-helpers-react";
 import Login from "../login";
+import Image from "next/image";
 
 export default function Index() {
   const session = useSession();
@@ -21,11 +22,29 @@ export default function Index() {
         <Login />
       ) : (
         <>
-          <div className=" bg-tertiary isolate">
+          <div className="bg-tertiary isolate">
             {/* <BackgroundBlobOrange /> */}
             <Header />
             <main className={Styles.main}>
-              <h1>Marketplace</h1>
+              <div className="grid overflow-hidden grid-cols-3 grid-rows-2 gap-2 h-auto mx-3 pt-16">
+                <div className="box row-span-2 flex flex-col">
+                  <h1 className={Styles.title}>Shopping Cart</h1>
+                  <a
+                    href="/marketplace/compare"
+                    className="justify-self-center self-center py-2.5 mb-4 mt-auto bg-white w-[90%] text-primary text-center text-middle rounded-lg font-semibold shadow-sm ring-1 ring-secondary hover:ring-secondary hover:bg-tertiary"
+                  >
+                    Compare
+                  </a>
+                </div>
+                <div className="box row-end-auto col-start-2 col-span-2">
+                  <h1 className={Styles.title}>
+                    The items will go here in individual boxes
+                  </h1>
+                </div>
+                <div className="box col-start-2 col-span-2 mt-16 pb-4">
+                  <h1 className={Styles.title}>Daily / Personalised Deals</h1>
+                </div>
+              </div>
               {/* <BackgroundBlobWhite /> */}
             </main>
             <Footer />
