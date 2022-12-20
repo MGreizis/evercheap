b01c8c20b67f5f17e3911a9734192d1231a5d1c1
JaneVick01 <ziak0001@hz.nl>
2022-12-20 11:04:09 +0100

    consume api for productboxes
    works:)

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Header from "../../components/header";
@@ -10,16 +9,15 @@ import Link from "next/link";
 import { useUser, useSupabaseClient, Session } from "@supabase/auth-helpers-react";
 import { Database } from "../../../utils/database.types";
 
-
 type Products = Database["public"]["Tables"]["products"]["Row"];
 type Product = {
     id:number;
     name:string;
+    deal: boolean;
 
 };
 
 export default function Index() {
-
     const session = useSession();
     const supabase = useSupabaseClient<Database>(); 
 
@@ -27,47 +25,41 @@ export default function Index() {
     useEffect(() => {  
         fetch("http://localhost:3010/stores/products")    
     .then(res => res.json())    
-    .then((data) => {
-        if (Array.isArray(data)){
-            setProducts(data);
+    .then((result) => {
 
-        } else {
-            setProducts([]);
-        }
+        setProducts(result.data);
+        // if (Array.isArray(data)){
+        //     setProducts(data);
+
+        // } else {
+        //     setProducts([]);
+        // }
       })    
-            .catch(() => {      // Handle error      
-                setProducts([]); // Ensure that products is always an array    
-            }); 
+        .catch(() => {      // Handle error      
+        setProducts([]); // Ensure that products is always an array    
+        }); 
         }, [])
-
 
 
     type ProductProps = {
         children: React.ReactNode;
     }
 
-    function ProductBox({ children }: ProductProps) { 
+
+    function ProductBoxes({ children }: ProductProps) { 
         return (
-        <div className={Styles.box}> 
-        
-        <ul>
-            {console.log(products)}
-             {products.length === 0 || products.includes(null!) || products.includes(undefined!) ? (
-        <li>No products to display</li>
-        ) : (
+            <>
+            {
             products.map(product => (
-            <li key={product.id}> 
-            <div> 
+                <div  className={Styles.box} key={product.id}> 
                 <h1>{product.name}</h1> 
                 <h1>{product.id}</h1> 
-                </div> 
-                </li>)) as JSX.Element[]
-                )} 
-                </ul>
-                {children}
-                </div>
-                ); 
-                }
+                </div>)) as JSX.Element[]
+            }
+            </> 
+        ); 
+    }
+
     return (
         <>
             <Head>
@@ -88,60 +80,22 @@ export default function Index() {
                                 <Link
                                     href="/marketplace/compare"
                                     className="
-                justify-self-center self-center py-2.5 mb-4 mt-auto bg-white w-[90%] text-primary text-center text-middle rounded-lg font-semibold shadow-sm hover:ring-secondary hover:text-secondary
-                rounded-full px-4 py-2 focus:outline-none focus:shadow-outline"
+                    justify-self-center self-center py-2.5 mb-4 mt-auto bg-white w-[90%] text-primary text-center text-middle rounded-lg font-semibold shadow-sm hover:ring-secondary hover:text-secondary
+                    rounded-full px-4 py-2 focus:outline-none focus:shadow-outline"
                                 >
                                     Compare
                                 </Link>
                             </div>
                             <div className={Styles.productbox}>
-                                <div className={Styles.gridlayout}>
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
-
-                                    <ProductBox>
-                                    </ProductBox>
+                                <div className={Styles.gridlayout}> 
+                                <ProductBoxes>
+                                </ProductBoxes>                   
+                                    
                                 </div>
                             </div>
                             <div className={Styles.dealsbox}>
-                                <ProductBox>
-                                </ProductBox>
-
-                                <ProductBox>
-                                </ProductBox>
-
-                                <ProductBox>
-                                </ProductBox>
+                                <ProductBoxes>
+                                </ProductBoxes>
                             </div>
                         </div>
                     </main>
